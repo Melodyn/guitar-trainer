37f6a19f5b0f31d8a48ad83d2214153bb204fee3
Sergei Melodyn <samelodyn@gmail.com>
2023-08-14 19:19:10 +0300

    release

--- src/utils.ts
+++
@@ -1,5 +0,0 @@
-export const find = <I>(arr: I[], callback: (item: I) => boolean): I => {
-  const item = arr.find(callback);
-  if (item !== undefined) return item;
-  throw new Error('Array has not contains this item');
-};
